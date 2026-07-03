8d21138f64d0eb9a35fbff93eb2e703859944d6b
Maria Eduarda de Almeida <dudacsantos450@gmail.com>
2026-07-03 14:41:19 -0300

    eat: estrutura inicial do projeto e dependências
    Configuração inicial do projeto Expo React Native com grupos de rotas ((auth), (aluno), (professor), (instituicao)) e instalação da biblioteca lucide-react-native.
    - Exemplo simples de redirecionamento com o `index.tsx` para pasta `(auth)`

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# mental_tech
+# Mental Tech
+
+```
+src/
+└── app/
+    ├── (auth)/             # Área de autenticação
+    │   └──  _layout.tsx
+    ├── (aluno)/            # Área do Aluno
+    │   └──  _layout.tsx    # Pode ter um Bottom Tab Navigator, por exemplo
+    │
+    ├── (professor)/        # Área do Professor
+    │   └──  _layout.tsx    # Pode ter um Bottom Tab Navigator, por exemplo
+    │
+    ├── (instituicao)/      # Área da Instituição/Admin
+    │   └──  _layout.tsx    # Pode ter um Bottom Tab Navigator, por exemplo
+    │
+    ├── _layout.tsx         # Layout Raiz (Root Layout)
+    └── index.tsx           # Tela de Redirecionamento (Splash/Check de Autenticação)
---
+++ src/app/(auth)/login.tsx
@@ -0,0 +1,51 @@
+﻿import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Login() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Login</Text>
+            <Text style={styles.subtitle}>Tela visual simples para teste.</Text>
+            <Link href="/cadastro" style={styles.link}>
+                <Text style={styles.linkText}>Ir para Cadastro</Text>
+            </Link>
+            <Link href="/esqueci_senha" style={styles.link}>
+                <Text style={styles.linkText}>Esqueci minha senha</Text>
+            </Link>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 24,
+        backgroundColor: "#fff",
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: "700",
+        marginBottom: 12,
+    },
+    subtitle: {
+        fontSize: 16,
+        color: "#444",
+        marginBottom: 24,
+        textAlign: "center",
+    },
+    link: {
+        width: "100%",
+        paddingVertical: 14,
+        paddingHorizontal: 18,
+        marginBottom: 12,
+        backgroundColor: "#007AFF",
+        borderRadius: 10,
+        alignItems: "center",
+    },
+    linkText: {
+        color: "#fff",
+        fontWeight: "600",
+    },
+});

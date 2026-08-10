3f1b27787bda9bc16cc1d4b0b70e4c6d45fafa3b
Madu Almeida <dudacsantos450@gmail.com>
2026-08-09 21:32:28 -0300

    Update auth

--- src/app/(auth)/professor/login.tsx
+++ src/app/(auth)/professor/login.tsx
@@ -96,8 +96,6 @@ export default function EducatorLoginRoute() {
         {!!loginError && <Text style={styles.errorText}>{loginError}</Text>}
       </View>
 
-      <View style={styles.separator} />
-
       <View style={styles.loginLinks}>
         <TouchableOpacity
           onPress={() =>
